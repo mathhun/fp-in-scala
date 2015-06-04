1bdcbf48d8079748fc70531d61943270973747df
mathhun <achemer@gmail.com>
2015-06-04 22:54:02 +0900

    chapter 11

--- src/main/scala/ch11/monad.scala
+++ src/main/scala/ch11/monad.scala
@@ -38,4 +38,39 @@ object Monad {
   //  price <- Gen.nextDouble
   //  quantity <- Gen.nextInt
   //} yield Order(Item(name, price), quantity)
+
+  //
+  // The Identity monad
+  //
+  case class Id[A](value: A)
+
+  //
+  // The State monad
+  //
+
+  case class State[S,A](run: S => (A, S)) {
+    def map[B](f: A => B): State[S, B] =
+      State(s => {
+        val (a, s1) = run(s)
+        (f(a), s1)
+      })
+    def flatMap[B](f: A => State[S, B]): State[S, B] =
+      State(s => {
+        val (a, s1) = run(s)
+        f(a).run(s1)
+      })
+  }
+
+  // type IntState[A] = State[Int, A]
+  // object IntStateMonad extends Monad[IntState] {
+  //   def unit[A](a: => A): IntState[A] = State(s => (a, s))
+  //   def flatMap[A, B](st: IntState[A])(f: A => IntState[B]): IntState[B] =
+  //     st flatMap f
+  // }
+
+  // def stateMonad[S] = new Monad[({type lambda[x] = State[S, x]})#lambda] {
+  //   def unit[A](a: => A): State[S, A] = State(s => (a, s))
+  //   def flatMap[A, B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+  //     st flatMap f
+  // }
 }
--- src/test/scala/ch11/monad.scala
+++ src/test/scala/ch11/monad.scala
@@ -12,4 +12,8 @@ class MonadSpec extends FlatSpec with Matchers {
 
   "#10" should "implement compose" ignore {
   }
+
+  "A monad" should "be an implementation of one of the minimal sets of monadic combinators, satisfying the laws of associativity and identity" in {
+    // Nothing
+  }
 }
