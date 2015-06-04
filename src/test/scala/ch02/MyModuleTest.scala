558fa775dcd2a3f1c62e7d3a345e545bd0621338
mathhun <achemer@gmail.com>
2015-06-05 07:54:43 +0900

    chapter 02

--- src/test/scala/ch02/MyModuleTest.scala
+++ src/test/scala/ch02/MyModuleTest.scala
@@ -10,4 +10,21 @@ class MyModuleSpec extends FlatSpec with Matchers {
     factorial(4) should be (24)
     factorial(10) should be (3628800)
   }
+
+  "#1 fib" should "compute Nth fibonacci number" in {
+    Range(0,10).map(fib) should be (Vector(0,1,1,2,3,5,8,13,21,34))
+    fib(30) should be (832040)
+  }
+
+  "#2 isSorted" should "checks whether an Array[A] is sorted" in {
+    isSorted(Array(0,1,2,3,4), (x: Int, y: Int) => x > y) should be (true)
+    isSorted(Array(1,2,0,3,4), (x: Int, y: Int) => x > y) should be (false)
+  }
+
+  "#3 partial1" should "be a higher-order function doing partial application" in {
+    val f = partial1(13, (x: Int, y: Int) => x + y)
+    f(0) should be (13)
+    f(1) should be (14)
+    f(2) should be (15)
+  }
 }
